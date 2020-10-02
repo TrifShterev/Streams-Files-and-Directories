fd3c16433db1f229b4f6101ddd3bd0f740583b24
TrifShterev <tshterev@yahoo.com>
2020-10-02 18:21:54 +0300

    added tasks

---
+++ Even Lines/EvenLines.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Even_Lines
+{
+    class EvenLines
+    {
+        static void Main(string[] args)
+        {
+            //1. Read a file
+           using StreamReader reader= new StreamReader("../../../text.txt");
+            using var writer = new StreamWriter("../../../output.txt");
+            string line = reader.ReadLine();
+            int lineCounter = 0;
+
+            while (line!=null)
+            {
+                if (lineCounter%2==0)
+                {
+                    Regex pattern = new Regex("[-,.'?!]");
+                    line = pattern.Replace(line,"@");
+
+                    var array = line.Split().ToArray().Reverse();
+                    writer.WriteLine(string.Join(" ",array));
+                }
+                lineCounter++;
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
---
+++ LineNumbers/LineNumbers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LineNumbers
+{
+    class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("../../../TextFile1.txt");
+
+            string[] result = new string[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                int countOfLetters = CountOfLetters(line);
+                int countOfMarks = CountOfPunctuationMarks(line);
+
+                result[i] = $"Line {i + 1}: {lines[i]}({countOfLetters})({countOfMarks})";
+            }
+
+            File.WriteAllLines("../../../output.txt", result);
+
+
+        }
+        static int CountOfLetters(string line)
+        {
+            int counter = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                char symbol = line[i];
+                if (char.IsLetter(symbol))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        static int CountOfPunctuationMarks(string line)
+        {
+            int counter = 0;
+            char[] punctuationMarks = { '-', ',', '.', '\'', '?', '!' };
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char symbol = line[i];
+                if (punctuationMarks.Contains(symbol))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
